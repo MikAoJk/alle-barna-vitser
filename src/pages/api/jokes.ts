d5824a872585c621d9df6e32358172632d99daa5
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-06-09 11:56:09 +0200

    Use api url if production

--- src/pages/api/jokes.ts
+++ src/pages/api/jokes.ts
@@ -11,10 +11,20 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Array<Joke>>
 ) {
+    if (process.env.NODE_ENV !== 'production') {
         const jsonDirectory = path.join(process.cwd(), 'public');
         const json = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
 
-        const jokes: Array<Joke> = JSON.parse(json);
+        const quotes: Array<Joke> = JSON.parse(json);
 
-        res.status(200).json(jokes);
+        res.status(200).json(quotes);
+    } else {
+        const urlApi = `https://mikaojk.github.io/alle-barna-vitser/data.json`
+
+        const result = await fetch(urlApi, {
+            method: req.method
+        })
+
+        res.status(result.status).json(await result.json());
+    }
 }
